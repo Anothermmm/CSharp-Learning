288bde3c7de19397aa8f2a3d72fb3c6048fff4b7
Anothermmm <51039117+Anothermmm@users.noreply.github.com>
2019-07-21 08:57:35 +0800

    Add files via upload

---
+++ 事件的实现.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    class TestEvent
+    {
+        public delegate void MyEventHandler();//为事件定义委托
+        public event MyEventHandler Click;//用event关键字声明
+        public void Trigger()//定义引发事件时调用的方法
+        {
+            Click();
+        }
+        public void M1()
+        {
+            Console.WriteLine("Hello!");
+        }
+        public void M2()
+        {
+            Console.WriteLine("Hello again!");
+        }
+        public void M3()
+        {
+            Console.WriteLine("Good_bye!");
+        }
+        static void Main()
+        {
+            TestEvent myEvent = new TestEvent();
+            myEvent.Click += new MyEventHandler(myEvent.M1);
+            myEvent.Click += new MyEventHandler(myEvent.M2);
+            myEvent.Click += new MyEventHandler(myEvent.M3);
+            //引发事件
+            myEvent.Trigger();
+            myEvent.Click -= new MyEventHandler(myEvent.M2);
+            Console.WriteLine("不再引发\"Hello again\"");
+            //重新引发事件
+            myEvent.Trigger();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 接口的实现.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    public delegate string MyDelegate(string name);//声明一个委托类型,像一个没有方法体的方法
+    public class Program
+    {
+        public static string FunctionA(string name)//定义被委托调用的方法，参数类型、个数、顺序都必须和delegate相同
+        {
+            return "A say Hello to" + name;
+        }
+        public static string FunctionB(string name)
+        {
+            return "B say Hello to" + name;
+        }
+        //定义delegate类型的处理函数，并在此函数中通过delegate类型调用步骤定义的方法
+        public static void MethodA(MyDelegate Me)
+        {
+            Console.WriteLine(Me("张三"));
+        }
+        public static void Main()
+        {
+            //创建实例，传入准备调用的方法名
+            MyDelegate a = new MyDelegate(FunctionA);
+            MyDelegate b = new MyDelegate(FunctionB);
+            MethodA(a);
+            MethodA(b);
+            Console.WriteLine();
+        }
+    }
+}
